bb1b1da2b2ea6b2a045b591ed9c0ad6705d2e150
Rails Girls Pune <railsgirlspune@gmail.com>
2013-08-23 18:22:09 +0530

    Hephzibah first checkin

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Blog::Application.config.secret_key_base = 'f1f7bf261cbacefcd28d80a1d331b60d3c46ced05d58e3ac867cdb76f08867dbcdcbc2f1a2d89a4f27e4600520bd5fb0d4bafc0a4928cd4f75d2353920b25d90'
